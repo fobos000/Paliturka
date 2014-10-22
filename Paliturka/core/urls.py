3898878bfef4db08123c4bb705fa3f3f8bf65e26
yhulpa <yuriy.hulpa@perfectial.com>
2014-10-22 18:42:35 +0300

    Added basic view for Add Book page. new_book blank template

--- Paliturka/core/urls.py
+++ Paliturka/core/urls.py
@@ -4,5 +4,5 @@ from core import views
 urlpatterns = patterns('',
 
     url(r'^$', views.IndexView.as_view()),
-
+    url(r'^new_book$', views.CreateBookView.as_view()),
 )
--- Paliturka/core/views.py
+++ Paliturka/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class CreateBookView(FormView):
+    template_name = 'new_book.html'
