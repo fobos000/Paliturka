5e288e847e0853c7ca902a2be9da2530e729af40
yhulpa <yuriy.hulpa@perfectial.com>
2014-10-23 18:01:55 +0300

    Show new books on home page.

--- Paliturka/core/views.py
+++ Paliturka/core/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
+from models import Book
+from datetime import datetime
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        today_books = Book.objects.filter(time_created=datetime.today())
+        kwargs['today_books'] = today_books
+        return kwargs
 
 class CreateBookView(FormView):
     template_name = 'new_book.html'
