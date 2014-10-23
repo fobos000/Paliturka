b580f24e06d6c2a2c000638bc97671764c7cdb55
yhulpa <yuriy.hulpa@perfectial.com>
2014-10-23 18:00:38 +0300

    Removed Author and genre models.

--- Paliturka/core/models.py
+++ Paliturka/core/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from django.db import models
 from django_facebook.models import FacebookCustomUser
 # Create your models here.
@@ -20,15 +19,21 @@ class Book(models.Model):
     status = models.CharField(max_length=4,
                               choices=BOOK_STATUS,
                               default=WILD)
-    rate = models.DecimalField(blank=True, decimal_places=3, max_digits=3)
-    imdb = models.URLField(blank=True)
-    image = models.ImageField(blank=True)
-    feedback = models.ForeignKey('Feedback')
-    history = models.ForeignKey('BookHistory')
+    rate = models.DecimalField(blank=True, null=True, decimal_places=3, max_digits=3)
+    imdb = models.URLField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+    feedback = models.ForeignKey('Feedback', null=True, blank=True)
+    history = models.ForeignKey('BookHistory', null=True)
     shelf = models.ForeignKey('Shelf')
     genre = models.ForeignKey('Genre')
-    publisher = models.ForeignKey('Publisher')
-    author = models.ForeignKey('Author')
+    publisher = models.CharField(max_length=50, blank=True, null=True)
+    author = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=14, blank=True, null=True)
+    country = models.ForeignKey('Country', blank=True, null=True)
+    time_created = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return ' '.join((self.name, self.author))
 
 
 class BookHistory(models.Model):
@@ -50,6 +55,9 @@ class Country(models.Model):
     code = models.CharField(max_length=4)
     flag = models.ImageField(blank=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Feedback(models.Model):
     """
@@ -57,17 +65,23 @@ class Feedback(models.Model):
     """
     user = models.ForeignKey(FacebookCustomUser, related_name='comments')
     comment = models.TextField(max_length=200)
-    time = datetime.now()
+    time = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.user
 
 
 class Shelf(models.Model):
     """
     Shelf models. Will indicate location of books in wild
     """
-    photo = models.ImageField(blank=True)
+    photo = models.ImageField(blank=True, null=True)
     address = models.CharField(max_length=50)
-    rate = models.DecimalField(blank=True, decimal_places=3, max_digits=3)
-    feedback = models.ForeignKey(Feedback)
+    rate = models.DecimalField(blank=True, decimal_places=3, max_digits=3, null=True)
+    feedback = models.ForeignKey(Feedback, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.address
 
 
 class Genre(models.Model):
@@ -76,16 +90,5 @@ class Genre(models.Model):
     """
     name = models.CharField(max_length=50)
 
-
-class Publisher(models.Model):
-    """
-    Book publisher model
-    """
-    name = models.CharField(max_length=50)
-
-
-class Author(models.Model):
-    """
-    Book author model
-    """
-    name = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.name
